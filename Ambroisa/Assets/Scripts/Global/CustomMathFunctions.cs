40d5a0e0d2bf52547541e1a93b34739c2455dbf9
Kertic <coolbman200@gmail.com>
2017-10-01 20:15:20 -0400

    Added custom math functions for us to put faster math functions as we need them, added friction, and platform dropping. Known bug (feature?) where the platform drop stops if they let go of <down> before passing all the way through.

---
+++ Ambroisa/Assets/Scripts/Global/CustomMathFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+
+public static class CustomMathFunctions
+{
+    
+    /// <summary>
+    /// Returns a -1 if the number is negitive, or a positive 1 otherwise
+    /// </summary>
+    /// <param name="value">The number to find the sign of</param>
+    /// <returns></returns>
+   public static float ReturnSign(float value)
+    {
+        if (value == 0)
+            return 1;
+
+
+        return (value / Math.Abs(value));
+
+    }
+
+
+}
--- Ambroisa/Assets/Scripts/Player/PlayerController.cs
+++ Ambroisa/Assets/Scripts/Player/PlayerController.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    //These are the class "PlayerController's" member variables.
     #region PlayerVariables
     #region Physics 
     public float vertVelocity;
     public float horizVelocity;
     public float weight;
+    public float frictionCoefficent;
     public float gravityAccel;
     public float runAccel;
     public float maxFallSpeed;
@@ -18,12 +19,25 @@ public class PlayerController : MonoBehaviour
 
     public bool grounded;
     #endregion
+    #region ButtonMasks
+    //These are turned on for the frame where a player Pressed the button, but off otherwise (including holding down the button)
+    bool jumpButton;
+    //These are turned on or off if the button is held. These are this way to enable controller support, where the directions arent buttons.
+    bool leftButton;
+    bool rightButton;
+    bool upButton;
+    bool downButton;
+    #endregion
     #region Controllability
     bool airJump;
     #endregion
+    #region External Objects
+    //This array keeps track of all objects we're in contact with
+    List<GameObject> currentCollisions;
+    #endregion
     #endregion
 
-    //Hi Brennan this is a test. 
+
 
     // Use this for initialization
     void Start()
@@ -46,7 +60,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-
+        #region Set Button Masks
+        jumpButton = Input.GetButtonDown("Jump");
+        rightButton = Input.GetAxis("Horizontal") > 0;
+        leftButton = Input.GetAxis("Horizontal") < 0;
+        upButton = Input.GetAxis("Vertical") > 0;
+        downButton = Input.GetAxis("Vertical") < 0;
+        #endregion
 
 
 
@@ -65,40 +85,82 @@ public class PlayerController : MonoBehaviour
         else
         #region Ground Physics
         {
+            #region Friction
+            if (!rightButton && !leftButton)//We aren't moving left or right
+            {
 
+                Debug.Log("Decel");
+                int direction = (int)CustomMathFunctions.ReturnSign(horizVelocity);
+                horizVelocity -= direction * frictionCoefficent;
+
+                if (direction > 0 && horizVelocity < 0)
+                    horizVelocity = 0;
+
+                if (direction < 0 && horizVelocity > 0)
+                    horizVelocity = 0;
+
+            }
+
+
+
+            #endregion
         }
         #endregion
 
 
         #region Controlling
         #region Jumping
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (jumpButton && grounded)
         {
-            Debug.Log("Jumping");
+            
             vertVelocity += jumpPower;
 
         }
-        else if (Input.GetButtonDown("Jump") && !grounded && airJump)
+        else if (jumpButton && !grounded && airJump)
         {
-            Debug.Log("Double Jumping");
+           
             vertVelocity = 0.0f;//We set it to 0 because it feels better to just instantly get full force jumps
             vertVelocity += jumpPower;
             airJump = false;
         }//No else, because if we arent grounded and we dont have an air jump, we can't jump
         #endregion
-        #region Running
-        if (Input.GetAxis("Horizontal") > 0)
+        #region Climb/Decending
+        if (downButton)
         {
-            Debug.Log("Moving right");
+            GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
+            for (int i = 0; i < platforms.Length; i++)
+            {
+                Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), platforms[i].GetComponent<Collider2D>(), true);
+            }
+
+        }
+        else
+        {
+            GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
+            for (int i = 0; i < platforms.Length; i++)
+            {
+                Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), platforms[i].GetComponent<Collider2D>(), false);
+            }
+        }
+        #endregion
+        #region Running
+        if (rightButton)
+        {
+            if (horizVelocity < 0)
+                horizVelocity = 0;
+
+           
             horizVelocity += runAccel;
             if (horizVelocity > maxRunSpeed)
             {
                 horizVelocity = maxRunSpeed;
             }
         }
-        else if (Input.GetAxis("Horizontal") < 0)
+        else if (leftButton)
         {
-            Debug.Log("Moving left");
+            if (horizVelocity > 0)
+                horizVelocity = 0;
+            
             horizVelocity -= runAccel;
             if (horizVelocity < (0.0f - maxRunSpeed))
             {
@@ -111,7 +173,7 @@ public class PlayerController : MonoBehaviour
         //Apply motion
         transform.Translate(Vector3.right * horizVelocity * Time.deltaTime);
         transform.Translate(Vector3.up * vertVelocity * Time.deltaTime);
-        
+
 
     }
 
@@ -120,17 +182,18 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Floor")
+       
+        
+        if (collision.gameObject.tag == "Floor" || collision.gameObject.tag == "Platform")
         {
 
             if (transform.position.y >= collision.gameObject.transform.position.y)//If this is true, we have landed
             {
-                Debug.Log("Landed");
-                Debug.Log(transform.position);
-                Debug.Log("Object: " + collision.gameObject.transform.position);
+                
+            
                 grounded = true;
                 vertVelocity = 0.0f;
-                airJump = true;//New test
+                airJump = true;
             }
 
             else
@@ -143,10 +206,12 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Floor")
+        
+
+        if (collision.gameObject.tag == "Floor" || collision.gameObject.tag == "Platform")
         {
             grounded = false;
-            Debug.Log("Leaving ground");
+            
         }
     }
 }
