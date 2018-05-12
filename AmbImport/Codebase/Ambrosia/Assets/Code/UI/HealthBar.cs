93ce04aded949862966ca25fc463adad8f3dd657
Kertic <coolbman200@gmail.com>
2018-05-12 14:28:18 -0400

    Imported Ambroisa as a subtree

---
+++ AmbImport/Codebase/Ambrosia/Assets/Code/UI/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    [SerializeField]
+    UnityEngine.UI.RawImage HealthBarFill;
+    [SerializeField]
+    UnityEngine.UI.RawImage HealthBarBackdrop;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetHealthBarAmount(float ratio)
+    {
+        HealthBarFill.rectTransform.sizeDelta = new Vector2(HealthBarBackdrop.rectTransform.sizeDelta.x * ratio, HealthBarBackdrop.rectTransform.sizeDelta.y);
+    }
+}
